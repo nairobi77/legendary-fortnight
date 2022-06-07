d03dba56b018308f5d01ea44462a71d7e3d817be
Aleksandr Kurakin <a.kurakin@cian.ru>
2022-06-08 00:32:49 +0300

    initial commit

---
+++ src/test/kotlin/com/example/legendaryfortnight/LegendaryFortnightApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.legendaryfortnight
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LegendaryFortnightApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
